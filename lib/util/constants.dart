820f17aba45d81efc2d4c4a6aaa0e977f0eeb534
mmarianosilva <mmarianosilva0@gmail.com>
2018-07-03 07:47:15 -0700

    Changed build setting

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static final version = "1.0.13";
+  static final version = "1.0.15";
   static final sentry_dsn = "https://babc462ab7dd49809c5ef1af185346c0@sentry.io/1236103";
 }
