6b5fd66d0ab7706b964020ddfad275a27720bd53
mmarianosilva <mmarianosilva0@gmail.com>
2018-08-04 10:09:59 -0700

    Build 1.1.21

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static final version = "1.1.20";
+  static final version = "1.1.21";
   static final sentry_dsn = "https://babc462ab7dd49809c5ef1af185346c0@sentry.io/1236103";
   static final oneDay = 1000 * 60 * 60 * 24;
 
