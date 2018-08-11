5fa5596f3702c995df1bbf8ccb72f06853205286
mmarianosilva <mmarianosilva0@gmail.com>
2018-08-10 23:30:04 -0700

    Added Versions

---
+++ scripts/build_ios_dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl https://storage.googleapis.com/flutter_infra/releases/beta/macos/flutter_macos_v0.5.1-beta.zip -o flutter_macos_v0.5.1-beta.zip
+unzip flutter_macos_v0.5.1-beta.zip
+export PATH=`pwd`/flutter/bin:$PATH
+cd ios && pod install
+cd .. && flutter doctor
+flutter build ios --release
