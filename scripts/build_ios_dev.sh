ff358912c66bb00ece52d44f625908179159ce4e
mmarianosilva <mmarianosilva0@gmail.com>
2018-08-11 17:30:57 -0700

    Added iOS Build

--- scripts/build_ios_dev.sh
+++ scripts/build_ios_dev.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-#TODO Env Add Configs and Set Build Number
+BUILD_NUM="$((${CIRCLE_BUILD_NUM}+1))"
+
+echo ${BUILD_NUM}
+
+cp ./scripts/dev/GoogleServices-Info.plist ./ios/Runner/GoogleServices-Info.plist
 
 curl https://storage.googleapis.com/flutter_infra/releases/beta/macos/flutter_macos_v0.5.1-beta.zip -o flutter_macos_v0.5.1-beta.zip
 unzip flutter_macos_v0.5.1-beta.zip
 export PATH=`pwd`/flutter/bin:$PATH
+
+flutter upgrade
+flutter doctor
+
 cd ios && pod install
-cd .. && flutter doctor
-flutter build ios --release
+cd ..
+flutter build ios --release --no-codesign --build-number="${BUILD_NUM}"
 cd ios && fastlane beta
