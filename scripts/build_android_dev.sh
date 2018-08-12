fbf5af69564d20d34927892b2c104f540ba469f4
mmarianosilva <mmarianosilva0@gmail.com>
2018-08-11 17:01:49 -0700

    Add Sudo

--- scripts/build_android_dev.sh
+++ scripts/build_android_dev.sh
@@ -6,9 +6,9 @@ echo ${BUILD_NUM}
 
 cp ./scripts/dev/google-services.json ./android/app/google-services.json
 
-gem install fastlane -NV
+sudo gem install fastlane -NV
 flutter upgrade
 flutter doctor
 flutter build apk --release --build-number=${BUILD_NUM}
-cd android && fastlane add_plugin appcenter
+cd android && sudo fastlane add_plugin appcenter
 fastlane deploy_beta
