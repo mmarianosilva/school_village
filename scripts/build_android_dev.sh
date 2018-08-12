fd488d4968a6af686e4721512b7930e9aa5c04e4
mmarianosilva <mmarianosilva0@gmail.com>
2018-08-11 17:09:22 -0700

    Version Number

--- scripts/build_android_dev.sh
+++ scripts/build_android_dev.sh
@@ -9,6 +9,6 @@ cp ./scripts/dev/google-services.json ./android/app/google-services.json
 sudo gem install fastlane -NV
 flutter upgrade
 flutter doctor
-flutter build apk --release --build-number=${BUILD_NUM}
+flutter build apk --release --build-number="${BUILD_NUM}"
 cd android && sudo fastlane add_plugin appcenter
 fastlane deploy_beta
