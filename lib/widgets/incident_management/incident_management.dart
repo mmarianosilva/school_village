cc6db97ad9b19fa6f7f8c0192bce786a8da994a5
mmarianosilva <mmarianosilva0@gmail.com>
2019-07-11 12:25:29 +0200

    Implemented new design for IncidentManagementScreen

--- lib/widgets/incident_management/incident_management.dart
+++ lib/widgets/incident_management/incident_management.dart
@@ -12,6 +12,8 @@ import 'package:scoped_model/scoped_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import 'incident_message.dart';
+
 
 class IncidentManagement extends StatefulWidget {
   final GlobalKey<_IncidentManagementState> key;
@@ -25,6 +27,15 @@ class IncidentManagement extends StatefulWidget {
 }
 
 class _IncidentManagementState extends State<IncidentManagement> {
+  static const titles = ["Message", "Message", "Alert"];
+  static const authors = ["Samuel Security", "Michael Wiggins", "Demo Staff"];
+  static const messages = [
+    "We have a report from the Anonymous Hotline that Tommy Jones, wearing a black hoodie and blue pants has a gun and is planning to shoot someone at school. Advise if he is seen and watch campus entry points to prevent entry if possible.",
+    "A simulation of system communications during an armed assailant incident will begin in one minute. This is only a simulation.",
+    "An Armed Assailant has been reported at SchoolVillage Demo"];
+  static const timestamps = ["9:16 AM", "9:12 AM", "9:01 AM"];
+  static const targetGroups = ["Security/Admin", "Staff, Students, Family", ""];
+
   GoogleMapController _mapController;
   DocumentSnapshot _userSnapshot;
   bool _isLoading = true;
@@ -43,6 +54,10 @@ class _IncidentManagementState extends State<IncidentManagement> {
   }
 
   void _onSchoolMap() {
+
+  }
+
+  void _showStopAlert() {
 
   }
 
@@ -68,6 +83,15 @@ class _IncidentManagementState extends State<IncidentManagement> {
         _isLoading = false;
       });
     });
+  }
+
+  Widget _buildListItem(BuildContext context, int index) {
+    return IncidentMessage(
+      title: titles[index],
+      author: authors[index],
+      message: messages[index],
+      timestamp: timestamps[index],
+      targetGroup: targetGroups[index]);
   }
 
   @override
@@ -107,8 +131,9 @@ class _IncidentManagementState extends State<IncidentManagement> {
                         mapType: MapType.satellite,
                         myLocationButtonEnabled: false,
                         indoorViewEnabled: true,
+                        markers: Set<Marker>.from([Marker(markerId: MarkerId("123456789"), position: LatLng(33.902007, -118.1532139), infoWindow: InfoWindow(title: "Armed Assailant Alert", snippet: "Reported by Demo Staff at 9:01 AM"))]),
                       ),
-                      flex: 8,
+                      flex: 10,
                     ),
                     Flexible(child: Padding(
                       padding: const EdgeInsets.all(8.0),
@@ -116,114 +141,129 @@ class _IncidentManagementState extends State<IncidentManagement> {
                         onTap: () {launch("https://goo.gl/maps/omQy8JRpbV8CqvzV7");},
                         child: Text("14429 Downey Ave, Paramount, CA 90723, USA",
                             textAlign: TextAlign.center,
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.lightBlue)
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0, color: Colors.blue)
                         ),
                       ),
-                    ), flex: 1),
+                    ), flex: 2),
+                    Flexible(child:
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+                      child: Text("Armed Assailant Alert!",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0)),
+                    ),
+                        flex: 1),
                     Flexible(
-                      child: Row(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Alert type",
-                                textAlign: TextAlign.start,
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0)),
-                          ),
-                          Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: <Widget>[
-                                    Text("911 Callback: ",
-                                        textAlign: TextAlign.end,
-                                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0)),
-                                    Text("John Smith",
-                                        textAlign: TextAlign.end,
-                                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0)),
-                                    GestureDetector(
-                                      onTap: () { launch("tel://9491234567"); },
-                                      child: Padding(
-                                        padding: const EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 0.0),
-                                        child: Text("949-123-4568",
-                                            textAlign: TextAlign.end,
-                                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0, color: Colors.lightBlue)),
-                                      ),
-                                    )
-                                  ],
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(8.0, 4.0, 4.0, 4.0),
+                          child: Row(
+                            children: <Widget>[
+                              Text("911 Callback: ",
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0)),
+                              Text("John Smith",
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(fontSize: 14.0)),
+                              GestureDetector(
+                                onTap: () { launch("tel://9491234567"); },
+                                child: Padding(
+                                  padding: const EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 0.0),
+                                  child: Text("949-123-4568",
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(fontSize: 14.0, color: Colors.blue)),
                                 ),
                               )
-                          )
-                        ],
-                      ),
-                      flex: 1,
+                            ],
+                          ),
+                        ),
+                        flex: 1
                     ),
                     Flexible(child: Padding(
                       padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
                       child: Text("An Armed Assailant has been reported at Paramount High School",
                           textAlign: TextAlign.start,
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0)
+                          style: TextStyle(fontSize: 14.0)
                       ),
                     ), flex: 2),
                     Flexible(child: Padding(
                       padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
                       child: Row(
                         children: <Widget>[
-                          Text("Initial report by: ",
+                          Text("Reported by: ",
                               textAlign: TextAlign.start,
                               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0)
                           ),
                           Text("Michael Wiggins",
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0)),
+                              style: TextStyle(fontSize: 14.0)),
                           GestureDetector(
                               onTap: () { launch("tel://9492741789"); },
                               child: Padding(
                                 padding: const EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 0.0),
                                 child: Text("949-274-1709",
-                                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0, color: Colors.lightBlue)),
+                                    style: TextStyle(fontSize: 14.0, color: Colors.blue)),
                               )
+                          ),
+                          Expanded(
+                              child: Text("4:04 PM",
+                                  textAlign: TextAlign.end,
+                                  style: TextStyle(fontWeight: FontWeight.bold))
                           )
                         ],
                       ),
                     ), flex: 1),
                     Flexible(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                  child: GestureDetector(
-                                      child: Image.asset("assets/images/broadcast_btn.png", width: 64, height: 64),
-                                      onTap: _showBroadcast),
-                                  padding: EdgeInsets.all(8)),
-                              Spacer(),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: GestureDetector(
-                                  child: Container(
-                                    color: Colors.white,
-                                    padding: EdgeInsets.all(4.0),
-                                    child: Row(
-                                        crossAxisAlignment: CrossAxisAlignment.center,
-                                        children: <Widget>[
-                                          Image.asset("assets/images/logo.png", width: 64, height: 64),
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Text("School Map"),
-                                          )
-                                        ]),
-                                  ),
-                                  onTap: _onSchoolMap,
-                                ),
-                              )
-                            ]),
-                        flex: 2
+                        child: Container(
+                          color: Color.fromARGB(255, 121, 123, 128),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                    child: GestureDetector(
+                                        child: Image.asset("assets/images/group_message_btn.png", width: 64, height: 64),
+                                        onTap: _showBroadcast),
+                                    padding: EdgeInsets.all(8)),
+                                Spacer(),
+                                Container(
+                                    child: GestureDetector(
+                                        child: Image.asset("assets/images/stop_sign.png", width: 64, height: 64),
+                                        onTap: _showStopAlert),
+                                    padding: EdgeInsets.all(8)),
+                                Spacer(),
+                                Container(
+                                    child: GestureDetector(
+                                        child: Image.asset("assets/images/school_map_btn.png", width: 64, height: 64),
+                                        onTap: () { launch("https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=33.9014382,-118.1531176"); }),
+                                    padding: EdgeInsets.all(8)),
+                              ]),
+                        ),
+                        flex: 3
                     ),
-                    Expanded(
-                      child: Chat(
-                        conversation: _securityConversation,
-                        user: _userSnapshot,
-                        showInput: false,
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("SECURITY COMMUNICATIONS", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red)),
+                            Spacer(),
+                            Text("Time", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red))
+                          ],
+                        ),
                       ),
-                      flex: 8,
+                      flex: 1
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                      child: Divider(color: Colors.red),
+                    ),
+                    Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                          child: ListView.builder(
+                            itemBuilder: _buildListItem,
+                            itemCount: 3,
+                          ),
+                        ),
+                        flex: 10
                     )
                   ],
                 );
