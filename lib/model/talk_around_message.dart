be532636fb639382578bcf77b2393b69b129e9c5
mmarianosilva <mmarianosilva0@gmail.com>
2019-08-01 13:23:19 +0100

    Updated TalkAroundMessage model

--- lib/model/talk_around_message.dart
+++ lib/model/talk_around_message.dart
@@ -2,8 +2,9 @@ class TalkAroundMessage {
   final String message;
   final DateTime timestamp;
   final String author;
+  final String authorId;
   final double latitude;
   final double longitude;
 
-  TalkAroundMessage(this.message, this.timestamp, this.author, this.latitude, this.longitude);
+  TalkAroundMessage(this.message, this.timestamp, this.author, this.authorId, this.latitude, this.longitude);
 }
