4525f4793ae8aee5dc3f5bb164ee08fe1a49fa8c
mmarianosilva <mmarianosilva0@gmail.com>
2019-08-14 10:32:18 +0100

    Added endedAt field to schoolAlert model class

--- lib/model/school_alert.dart
+++ lib/model/school_alert.dart
@@ -6,6 +6,7 @@ class SchoolAlert {
   final String title;
   final String body;
   final DateTime timestamp;
+  final DateTime timestampEnded;
   final String createdBy;
   final String createdById;
   final LocationData location;
@@ -13,13 +14,14 @@ class SchoolAlert {
   final String reportedByPhone;
   final bool resolved;
 
-  SchoolAlert(this.id, this.title, this.body, this.timestamp, this.createdBy, this.createdById, this.location, this.type, this.reportedByPhone, {this.resolved = true});
+  SchoolAlert(this.id, this.title, this.body, this.timestamp, this.timestampEnded, this.createdBy, this.createdById, this.location, this.type, this.reportedByPhone, {this.resolved = true});
 
   SchoolAlert.fromMap(DocumentSnapshot firebaseModel) : this(
     firebaseModel.documentID,
     firebaseModel["title"],
     firebaseModel["body"],
     DateTime.fromMillisecondsSinceEpoch(firebaseModel["createdAt"]),
+    firebaseModel["endedAt"] != null ? DateTime.fromMicrosecondsSinceEpoch(firebaseModel["endedAt"].microsecondsSinceEpoch) : null,
     firebaseModel["createdBy"],
     firebaseModel["createdById"],
     LocationData.fromMap(Map<String, double>.from(firebaseModel["location"])),
