6a5bdb263efb80eb865197583caffe72ca6cc26e
mmarianosilva <mmarianosilva0@gmail.com>
2020-01-22 18:26:32 +0100

    Centralized date and time formatter

--- lib/util/date_formatter.dart
+++ lib/util/date_formatter.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
-final headerDateFormatter = DateFormat('EEEE, MMMM dd, yyyy');
-final messageDateFormatter = DateFormat('hh:mm a on MM/dd/yyyy');
+final DateFormat headerDateFormatter = DateFormat('EEEE, MMMM dd, yyyy');
+final DateFormat messageDateFormatter = DateFormat('hh:mm a on MM/dd/yyyy');
+final DateFormat timeFormatter = DateFormat("hh:mm a");
+final DateFormat dateFormatter = DateFormat("MMM dd, yyyy", "en_US");
 
 String getHeaderDate(millis) {
   var time = DateTime.fromMillisecondsSinceEpoch(millis);
