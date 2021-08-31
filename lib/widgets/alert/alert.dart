bf27b1f940e96cc46859b2e2457fe9ed9c015625
mmarianosilva <mmarianosilva0@gmail.com>
2021-08-31 07:22:27 +0530

    Testing Certificate

--- lib/widgets/alert/alert.dart
+++ lib/widgets/alert/alert.dart
@@ -254,7 +254,7 @@ class _AlertState extends State<Alert> {
       if (mEvent == EventAction.None) {
         return;
       }
-      //
+      //updated Cert
       //final String incidentUrl =
       //await _saveAlert(alertTitle,
       //alertBody, alertType, context);
